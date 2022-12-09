e70d5a10d4525bf80c46057c3a60723fb57c68fd
kkm5291 <kkm23125291@gmail.com>
2022-12-09 10:40:14 +0900

    feat: 로그인, 회원가입 기능 추가

---
+++ src/main/java/com/withtaxi/taxi/controller/LoginController.java
@@ -0,0 +1,55 @@
+package com.withtaxi.taxi.controller;
+
+
+import com.withtaxi.taxi.model.User;
+import com.withtaxi.taxi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/loginForm") // SecurityConfig 파일을 작성하면 스프링시큐리티의 자동 login 페이지 사용 불가능
+    public String loginForm() {
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm") // 회원가입 홈페이지로 이동
+    public String joinForm() {
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+
+        return "redirect:/loginForm";
+    }
+}
