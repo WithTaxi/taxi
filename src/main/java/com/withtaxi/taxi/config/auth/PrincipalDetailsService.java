0026c2a67f21f2ee58f5df9ba0d31686c6f692c4
kkm5291 <kkm23125291@gmail.com>
2022-12-09 17:31:28 +0900

    feat : 로그인 권한 기능 추가

---
+++ src/main/java/com/withtaxi/taxi/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,27 @@
+package com.withtaxi.taxi.config.auth;
+
+import com.withtaxi.taxi.model.User;
+import com.withtaxi.taxi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // 아이디 가져오는 것 (메서드 이름만 Username)
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        if (userEntity != null) {
+            return new PrincipalDetails(userEntity);
+        }
+        return null;
+    }
+}
