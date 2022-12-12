dfa88f81457057cd8f31a68f9a4a2ce2a661af73
kkm5291 <kkm23125291@gmail.com>
2022-12-13 00:14:53 +0900

    Feat : OAuth2User 로그인, 회원가입 기능 추가

--- src/main/java/com/withtaxi/taxi/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/withtaxi/taxi/config/oauth/PrincipalOauth2UserService.java
@@ -1,6 +1,7 @@
 package com.withtaxi.taxi.config.oauth;
 
 import com.withtaxi.taxi.config.auth.PrincipalDetails;
+import com.withtaxi.taxi.config.oauth.provider.*;
 import com.withtaxi.taxi.model.User;
 import com.withtaxi.taxi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -20,12 +23,27 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
-        String provider = userRequest.getClientRegistration().getRegistrationId(); // google
-        String providerId = oAuth2User.getAttribute("sub");
+        System.out.println("oAuth2User.getAttributes() : " + oAuth2User.getAttributes());
+        System.out.println("userRequest : " + userRequest.getClientRegistration().getRegistrationId());
+        OAuth2UserInfo oAuth2UserInfo = null;
+
+        if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+            oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("kakao")) {
+            oAuth2UserInfo = new KakaoUserInfo(oAuth2User.getAttributes());
+        }
+
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId = oAuth2UserInfo.getProviderId();
         String userId = provider + "_" + providerId;
         String password = "소셜 로그인";
-        String email = oAuth2User.getAttribute("email");
-        String name = oAuth2User.getAttribute("name");
+        String email = oAuth2UserInfo.getEmail();
+        String name = oAuth2UserInfo.getName();
+
 
         User userEntity = userRepository.findByUserId(userId);
 
