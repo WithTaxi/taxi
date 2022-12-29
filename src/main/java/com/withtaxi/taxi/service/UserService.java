029d4633e0d544d28fc0968f090fc5d9e551b629
kkm5291 <kkm23125291@gmail.com>
2022-12-29 18:26:11 +0900

    Feat : UserService 비즈니스 로직 추가
    registerUser
    removeUser
    getUserByUserId

--- src/main/java/com/withtaxi/taxi/service/UserService.java
+++ src/main/java/com/withtaxi/taxi/service/UserService.java
@@ -6,4 +6,11 @@ import com.withtaxi.taxi.model.User;
 public interface UserService {
 
     public User findId(String name, String email);
+
+    User getUserByUserId(String userId);
+
+    User registerUser(User user);
+
+
+    void removeUser(String userId);
 }
--- src/main/java/com/withtaxi/taxi/service/UserServiceImpl.java
+++ src/main/java/com/withtaxi/taxi/service/UserServiceImpl.java
@@ -3,13 +3,16 @@ package com.withtaxi.taxi.service;
 import com.withtaxi.taxi.model.User;
 import com.withtaxi.taxi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public User findId(String name, String email) {
@@ -22,5 +25,24 @@ public class UserServiceImpl implements UserService{
         }
 
         return result;
+    }
+
+    @Override
+    public User getUserByUserId(String userId) {
+        return userRepository.getUserByUserId(userId);
+    }
+
+    @Override
+    public User registerUser(User user) {
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        return userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void removeUser(String userId) {
+        userRepository.deleteByUserId(userId);
     }
 }
