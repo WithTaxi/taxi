94b4e41598128bb895eada8f4acd47d5ea7bca5d
kkm5291 <kkm23125291@gmail.com>
2022-12-29 18:28:30 +0900

    Refactor : UserController 추가 후 역할 분리
    CRUD 메서드 UserController로 이동

--- src/main/java/com/withtaxi/taxi/controller/LoginController.java
+++ src/main/java/com/withtaxi/taxi/controller/LoginController.java
@@ -1,36 +1,14 @@
 package com.withtaxi.taxi.controller;
 
-import com.withtaxi.taxi.config.auth.PrincipalDetails;
 import com.withtaxi.taxi.model.User;
-import com.withtaxi.taxi.repository.UserRepository;
+import com.withtaxi.taxi.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
-
-
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    /***
-     * user 정보 조회 API
-     * @param principalDetails
-     * @return principalDetails.getUser()
-     */
-    @GetMapping("/userInfo")
-    public @ResponseBody User user(@AuthenticationPrincipal PrincipalDetails principalDetails) {
-        return principalDetails.getUser();
-    }
 
     /***
      * 일반 로그인
@@ -48,19 +26,5 @@ public class LoginController {
     @GetMapping("/joinForm")
     public String joinForm() {
         return "joinForm";
-    }
-
-    /***
-     * 회원가입 로직
-     * @param user
-     * @return
-     */
-    @PostMapping("/join")
-    public String join(User user) {
-        String rawPassword = user.getPassword();
-        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
-        user.setPassword(encPassword);
-        userRepository.save(user);
-        return "redirect:/loginForm";
     }
 }
