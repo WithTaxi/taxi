8cf264f9472c741452034d2fa04357e7514cc6ac
kkm5291 <kkm23125291@gmail.com>
2023-01-03 17:43:25 +0900

    Feat : 유저 권한 추가

--- src/main/java/com/withtaxi/taxi/config/auth/PrincipalDetails.java
+++ src/main/java/com/withtaxi/taxi/config/auth/PrincipalDetails.java
@@ -3,18 +3,26 @@ package com.withtaxi.taxi.config.auth;
 import com.withtaxi.taxi.model.User;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
+/***
+ * UserDTO
+ */
 @Getter
 public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;
 
     private Map<String, Object> attributes;
+
+    private String ROLE_PREFIX = "ROLE_";
 
     public PrincipalDetails(User user) {
         this.user = user;
@@ -32,7 +40,9 @@ public class PrincipalDetails implements UserDetails, OAuth2User {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(ROLE_PREFIX + user.getRole()));
+        return authorities;
     }
 
     @Override
--- src/main/java/com/withtaxi/taxi/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/withtaxi/taxi/config/auth/PrincipalDetailsService.java
@@ -3,11 +3,14 @@ package com.withtaxi.taxi.config.auth;
 import com.withtaxi.taxi.model.User;
 import com.withtaxi.taxi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+/***
+ * 로그인 실행을 위한 클래스
+ */
 
 @Service
 @RequiredArgsConstructor
@@ -15,21 +18,20 @@ public class PrincipalDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    // 아이디 가져오는 것 (메서드 이름만 Username)
-
     /***
      * 로그인 메서드
-     * @param username the username identifying the user whose data is required.
+     * @param userId the username identifying the user whose data is required.
      * @return
      * @throws UsernameNotFoundException
      */
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User userEntity = userRepository.findByUserId(username);
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUserId(userId);
         if (userEntity != null) {
             return new PrincipalDetails(userEntity);
+        } else {
+            throw new UsernameNotFoundException("Not Found User");
         }
-        return null;
     }
 
 
