4b33ba00642a18e39c042351c1b2efd5cc4a7ca6
kkm5291 <kkm23125291@gmail.com>
2023-01-07 21:36:19 +0900

    Feat : 이메일 인증 서비스 구현

---
+++ src/main/java/com/withtaxi/taxi/config/EmailConfig.java
@@ -0,0 +1,36 @@
+package com.withtaxi.taxi.config;
+
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+
+    @Bean
+    public JavaMailSender javaMailService() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setUsername("kkm23125291@gmail.com");
+        javaMailSender.setPassword("gxnqegfeecujiouy");
+        javaMailSender.setPort(465);
+        javaMailSender.setJavaMailProperties(getMailProperties());
+        javaMailSender.setDefaultEncoding("UTF-8");
+        return javaMailSender;
+    }
+    private Properties getMailProperties() {
+        Properties pt = new Properties();
+        pt.put("mail.smtp.socketFactory.port", 465);
+        pt.put("mail.smtp.auth", "true");
+        pt.put("mail.smtp.starttls.enable", "true");
+        pt.put("mail.smtp.starttls.required", "true");
+        pt.put("mail.smtp.socketFactory.fallback","false");
+        pt.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        return pt;
+    }
+}
