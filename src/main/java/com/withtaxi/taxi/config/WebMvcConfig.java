9e931535c9eea0c66ccfec1e92e8a23bfbf7fbc4
kkm5291 <kkm23125291@gmail.com>
2023-01-11 01:45:17 +0900

    Refactor : WebConfig 수정

--- src/main/java/com/withtaxi/taxi/config/WebMvcConfig.java
+++ src/main/java/com/withtaxi/taxi/config/WebMvcConfig.java
@@ -2,31 +2,25 @@ package com.withtaxi.taxi.config;
 
 
 import org.springframework.boot.web.servlet.view.MustacheViewResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.persistence.Basic;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        MustacheViewResolver resolver = new MustacheViewResolver();
-        resolver.setCharset("UTF-8");
-        resolver.setContentType("text/html; charset=UTF-8");
-        resolver.setPrefix("classpath:/templates/");
-        resolver.setSuffix(".html");
-
-        registry.viewResolver(resolver);
-    }
-
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST")
+                .allowedMethods("*")
                 .allowCredentials(true)
                 .maxAge(3000);
+
     }
 }
