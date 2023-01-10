911a59f6010d0d08116ea3d541a37941a6517358
kkm5291 <kkm23125291@gmail.com>
2023-01-11 01:41:30 +0900

    Chore : response 변경

--- src/main/java/com/withtaxi/taxi/config/handler/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/withtaxi/taxi/config/handler/CustomAuthenticationFailureHandler.java
@@ -3,6 +3,7 @@ package com.withtaxi.taxi.config.handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -14,6 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /***
  * 로그인 실패 시 해당 클래스로 이동됨
@@ -26,11 +29,16 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        Map<String, Object> map = new HashMap<>();
+        map.put("result", 0);
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
+        ResponseEntity responseEntity = new ResponseEntity(map, HttpStatus.valueOf(response.getStatus()));
+
+
         System.out.println("로그인 실패");
 
-        objectMapper.writeValue(response.getWriter(), 0);
+        objectMapper.writeValue(response.getWriter(), responseEntity);
     }
 }
--- src/main/java/com/withtaxi/taxi/config/handler/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/withtaxi/taxi/config/handler/CustomAuthenticationSuccessHandler.java
@@ -4,18 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /***
@@ -36,8 +30,9 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
         response.setStatus(HttpStatus.OK.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
+
         System.out.println("로그인 성공");
 
-        objectMapper.writeValue(response.getWriter(), 1);
+        objectMapper.writeValue(response.getWriter(), response.getStatus());
     }
 }
