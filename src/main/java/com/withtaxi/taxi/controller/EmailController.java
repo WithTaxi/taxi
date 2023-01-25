28a7ebd3c77ed29ff0b5c97502a540a743d6bb37
kkm5291 <kkm23125291@gmail.com>
2023-01-25 22:25:37 +0900

    Feat : 이메일 임시 비밀번호 발급 및 임시비밀번호로 재설정 추가

--- src/main/java/com/withtaxi/taxi/controller/EmailController.java
+++ src/main/java/com/withtaxi/taxi/controller/EmailController.java
@@ -1,21 +1,44 @@
 package com.withtaxi.taxi.controller;
 
+import com.withtaxi.taxi.model.User;
+import com.withtaxi.taxi.repository.UserRepository;
 import com.withtaxi.taxi.service.EmailService;
+import com.withtaxi.taxi.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/join/email")
+@RequestMapping("api/email")
 @RequiredArgsConstructor
 public class EmailController {
 
     private final EmailService emailService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/mailConfirm")
     String mailConfirm(@RequestParam("email") String email) throws Exception {
         String code = emailService.sendSimpleMessage(email);
 
         return code;
+    }
+
+    @PostMapping("/issuedTemporaryPassword")
+    public int issueTemporaryPassword(@RequestParam("userId") String userId) throws Exception {
+
+        User user = userRepository.findByUserId(userId);
+        if (user == null) {
+            return 0;
+        }
+
+        String temporaryPassword = emailService.sendSimpleMessage(user.getEmail());
+
+
+        String encPassword = passwordEncoder.encode(temporaryPassword);
+        user.setPassword(encPassword);
+
+        userRepository.save(user);
+        return 1;
     }
 }
