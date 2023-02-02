4bdd3e48ae426e5abfcb86bac8b69d7d43e25690
kkm5291 <kkm23125291@gmail.com>
2023-02-02 12:46:32 +0900

    Feat : "/" 접근권한 permitall 추가

--- src/main/java/com/withtaxi/taxi/config/SecurityConfig.java
+++ src/main/java/com/withtaxi/taxi/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().cors();
         http.authorizeHttpRequests()
+                .antMatchers("/").permitAll()
                 .antMatchers("/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
