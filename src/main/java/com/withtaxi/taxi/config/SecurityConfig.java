7f5db303f855a1dd43751d76c0ec280ded539c2b
kkm5291 <kkm23125291@gmail.com>
2023-02-16 16:52:37 +0900

    Chore : 사용하지 않는 import 문 삭제

--- src/main/java/com/withtaxi/taxi/config/SecurityConfig.java
+++ src/main/java/com/withtaxi/taxi/config/SecurityConfig.java
@@ -14,9 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.servlet.Filter;
 
 
 @Configuration
